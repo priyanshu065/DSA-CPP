e19037deed005e8d859ce6e8bef36b55bdd1c984
priyanshu065 <priyanshuaditya239@gmail.com>
2026-08-22 23:27:36 +0530

    concept of null pointer

---
+++ C++/pointer/nullpointer.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+void changeA(int &p) { //pass by refrence using alias
+     p = 20;
+}
+
+int main(){
+    int a = 10;
+
+    changeA(a);
+    cout<<a<<endl;
+    return 0;
+}
