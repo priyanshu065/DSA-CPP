9ff823e3dca8c0b1a3a824be75ff411ba388f74a
priyanshu065 <priyanshuaditya239@gmail.com>
2026-09-04 00:18:09 +0530

    size of vector

---
+++ DSA/stacks.cpp/array/vectorarr/sizeofvec.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+
+
+int main () {
+    vector <int> vec = {1,2,3,4};
+    cout << vec[0] << endl;
+    cout << "size is " << vec.size();
+    return 0;
+}
